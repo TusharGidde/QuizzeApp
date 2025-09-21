e065dc4d2b585da2419561683b21934182755725
TusharGidde <tushargidde00730@gmail.com>
2025-09-21 21:25:35 +0530

    deploymenttt

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,20 +4,20 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'quiz_service2',
-  process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || 'root',
+  process.env.DB_NAME ,
+  process.env.DB_USER ,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
+    host: process.env.DB_HOST ,
+    port: process.env.DB_PORT ,
     dialect: 'mysql',
-    logging: process.env.NODE_ENV === 'development' ? console.log : false,
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
+    logging:false,
+    dialectOptions: {
+    ssl: {
+      minVersion: "TLSv1.2",
+      rejectUnauthorized: true, // set false if you run into cert issues
+    },
+  },
   }
 );
 
