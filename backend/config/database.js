bf52a51a871bb2aeba699190a66f0912e42a8dbf
TusharGidde <tushargidde00730@gmail.com>
2025-09-22 18:56:16 +0530

    deployment

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,23 +1,29 @@
-const { Sequelize } = require('sequelize');
-const dotenv = require('dotenv');
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME ,
-  process.env.DB_USER ,
+  process.env.DB_NAME,
+  process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST ,
-    port: process.env.DB_PORT ,
-    dialect: 'mysql',
-    logging:false,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 4000,
+    dialect: "mysql", // TiDB is MySQL-compatible
+    logging: false,
     dialectOptions: {
-    ssl: {
-      minVersion: "TLSv1.2",
-      rejectUnauthorized: true, // set false if you run into cert issues
+      ssl: {
+        minVersion: "TLSv1.2",
+        rejectUnauthorized: false, // <--- key change
+      },
     },
-  },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   }
 );
 
